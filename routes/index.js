7fcfac1cab575deb66241369dd9137d8d065a6d0
ViicVaporub <74691934+ViicVaporub@users.noreply.github.com>
2021-06-15 18:40:24 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const cors = require("cors");
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+const app = express(); //crear al servidor
+const port = process.env.PORT || 3080;
+app.use(cors())
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+
+module.exports = router;
+
+
+app.listen(port, () => {
+  console.log(`hola servidor ejecucion en http://localhost:${port}`);
+})
