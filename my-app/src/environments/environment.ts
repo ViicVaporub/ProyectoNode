2a228d8cf205beb6ca017f54b734b1c5b47eb847
Odracir2435 <ricardomunoz2435@gmail.com>
2021-06-16 00:03:11 -0500

    Update environment.ts
    Se agrego la key de fire base para los usuarios de login

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAR-X7sNS0CdYE5vJWZnGs0lrbr46Oe9os",
+    authDomain: "login-ng-338c1.firebaseapp.com",
+    projectId: "login-ng-338c1",
+    storageBucket: "login-ng-338c1.appspot.com",
+    messagingSenderId: "891791331641",
+    appId: "1:891791331641:web:05bbf37c749054df8e98e8"
+  },
 };
 
 /*
