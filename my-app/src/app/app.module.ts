97a74f4e2e15fa5f85368f85b4cf5b9379742776
ViicVaporub <74691934+ViicVaporub@users.noreply.github.com>
2021-06-15 19:19:42 -0500

    Navbar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { FaqComponent } from './faq/faq.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactanosComponent,
+    FaqComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
