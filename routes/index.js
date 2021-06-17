f13d0642fbb25ec83ad3559c021979f66aed457f
ViicVaporub <74691934+ViicVaporub@users.noreply.github.com>
2021-06-17 02:12:03 -0500

    fondo sheesh

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 const app = express(); //crear al servidor
-const port = process.env.PORT || 4020;
+const port = process.env.PORT || 3080;
 app.use(cors())
 
 app.use(express.urlencoded());
