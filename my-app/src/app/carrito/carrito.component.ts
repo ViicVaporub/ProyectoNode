8d1b962e2da49efb723381a89692fcf08d9ecbaa
Samuel-Reynoso <s.reynoso.hdz@gmail.com>
2021-06-18 02:32:11 -0500

    Carrito

--- my-app/src/app/carrito/carrito.component.ts
+++ my-app/src/app/carrito/carrito.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+
+import { FirebaseServiceService } from '../services/firebase-service.service';
 
 @Component({
   selector: 'app-carrito',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
+  config:any;
+  collection = { count:10, data:[] }
+
+  constructor(
+    private modalService: NgbModal,
+    public fb: FormBuilder,
+    private firebaseServiceService: FirebaseServiceService
+  ) { }
 
   ngOnInit(): void {
+    for(var i=0;i<this.collection.count;i++){
+      this.collection.data.push({
+        nombre: "nombre"+1,
+      tipo: "tipo"+1,
+      proveedor: "proveedor"+1,
+      valoractivo: "v"+1
+      })
+    }
+  }
+  eliminar(item:any):void{
+    this.collection.data.pop();
   }
 
 }
