b354a5ca5aac55038fad2058ee22079ae1538c32
Odracir2435 <ricardomunoz2435@gmail.com>
2021-06-18 07:43:24 -0500

    Update app.module.ts

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,10 +11,7 @@ import { ContactanosComponent } from './contactanos/contactanos.component';
 import { FaqComponent } from './faq/faq.component';
 import { TiendaComponent } from './tienda/tienda.component';
 import { FooterComponent } from './footer/footer.component';
-
 import { DataDbService } from './service/data-db.service';
-
-
 import { CrudComponent } from './crud/crud.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from 'src/environments/environment';
@@ -23,7 +19,6 @@ import { InventarioComponent } from './inventario/inventario.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { VentasComponent } from './ventas/ventas.component';
 import { TablaComponent } from './tabla/tabla.component';
-
 import {HttpClientModule} from '@angular/common/http';
 
 
